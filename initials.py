8c7ecc644eab34a83a0d8b9656a6c6313bb7902e
Spencer Ryan Stradling <128996139+Rstrad@users.noreply.github.com>
2024-03-19 10:48:07 -0400

    Add files via upload
    Write the code to output your first and last initials in ASCII format.

---
+++ initials.py
@@ -0,0 +1,10 @@
+#Ryan Stradling
+#I garden for fun!
+
+print("RRRR   SSS ")
+print("R   R S   S")
+print("R   R S    ")
+print("RRRR   SSS ")
+print( "R R       S")
+print("R  R  S   S")
+print("R   R  SSS ")
