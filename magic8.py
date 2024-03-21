5feb00b7ab53b3990241b20c70bf1e31834d25b7
Spencer Ryan Stradling <128996139+Rstrad@users.noreply.github.com>
2024-03-21 10:46:19 -0400

    Add files via upload
    Write a Magic 8-Ball program in Python that can answer any "Yes" or "No" question with a different fortune each time it executes. Include conditions for blank inputs to maintain proper output formatting.

---
+++ magic8.py
@@ -0,0 +1,46 @@
+#Write a Magic 8-Ball program in Python that can answer any "Yes" or "No" question with a different fortune each time it executes
+#Include conditions for blank inputs to maintain proper output formatting
+
+import random
+
+name = "Ryan" 
+question = "Am I living in a simulation?"
+answer = ""
+
+random_number = random.randint(1, 11)
+#print(random_number)
+
+if random_number == 1:
+  answer = "Yes - definitely"
+elif random_number == 2:
+  answer = "It is decidedly so"
+elif random_number == 3:
+  answer = "Without a doubt"
+elif random_number == 4:
+  answer = "Reply hazy, try again"
+elif random_number == 5:
+  answer = "Ask again later"
+elif random_number == 6:
+  answer = "Better not tell you now"
+elif random_number == 7:
+  answer = "My sources say no"
+elif random_number == 8:
+  answer = "Outlook not so good"
+elif random_number == 9:
+  answer = "Very doubtful"
+elif random_number == 10:
+  answer = "Exactly right"
+elif random_number == 11:
+  answer = "Not an expert on this"
+else:
+  answer = "Error"
+
+if len(name) == 0:
+  print("Question: " + question)
+else:
+  print(name + " asks: " + question)
+
+if len(question) == 0:
+  print("Error")
+else:
+  print("Magic 8-Ball's answer: " + answer)
